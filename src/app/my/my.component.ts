c580d69f13322ddd5cac7a8cf51a490473ec2cb8
tehwolf <robert.weyres@conciso.de>
2021-10-03 17:17:23 +0200

    Inject MyService into MyComponent

--- src/app/my/my.component.ts
+++ src/app/my/my.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { MyService } from '../my.service';
+
 @Component({
   selector: 'app-my',
   templateUrl: './my.component.html',
   styleUrls: ['./my.component.scss'],
 })
 export class MyComponent {
-  constructor() {}
+  constructor(private myService: MyService) {}
 }
