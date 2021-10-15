1fc0de1faecda914811c751c361c44f1e51120f4
tehwolf <robert.weyres@conciso.de>
2021-10-15 16:22:12 +0200

    Add API output to component

--- src/app/my/my.component.ts
+++ src/app/my/my.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { MyService } from '../my.service';
+import { Sample } from '../sample';
 
 @Component({
   selector: 'app-my',
   templateUrl: './my.component.html',
   styleUrls: ['./my.component.scss'],
 })
-export class MyComponent {
+export class MyComponent implements OnInit, OnDestroy {
+  multiSampleData$: Observable<Sample[]> | undefined;
+  singleSampleData$: Observable<Sample> | undefined;
   subComponentOutput = '';
 
+  private unsubscribe$ = new Subject<void>();
+
   constructor(private myService: MyService) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  fetchData(): void {
+    this.singleSampleData$ = this.myService
+      .getSampleDataById(0)
+      .pipe(takeUntil(this.unsubscribe$));
+    this.multiSampleData$ = this.myService
+      .getSampleData()
+      .pipe(takeUntil(this.unsubscribe$));
+  }
 
   writeOutput(outputValue: string): void {
     this.subComponentOutput = outputValue;
